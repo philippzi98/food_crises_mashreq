933ef86adecc5b1381f2e7b35de0c4b840ae2c22
dominik.wielath <dominik.wielath@gmail.com>
2024-09-30 18:31:00 +0200

    Changing the pipeline function.

--- monthly-update/pipeline.sh
+++ monthly-update/pipeline.sh
@@ -1,42 +1,130 @@
 #!/bin/bash
 
-# Setting variables
-input_file_path="../../data/final/downloads-newsapi/Mashreq_2024-06-23_2024-07-24_articles_eng.csv"
-output_file_folder="../../data/final/processing1-counts-per-article/"
-language="English"
-
-# Extract the file name from the input path (basename removes the directory path)
-input_file_name=$(basename "$input_file_path")
+# Define the folder to search in
+input_folder="../../data/monthly-updates/1_downloads_newsapi"  
+output_processing_1_folder="../../data/monthly-updates/2_processing-counts-per-article"  
+output_processing_2_folder="../../data/monthly-updates/3_processing-counts-per-day"  
+final_output_folder="../../data/monthly-updates/4_final-output"  
 
 
 #################################################
-# 1. count_keywords_per_article.py 
+# 1. Check for unprocessed files
 #################################################
 
-# Replace .csv with _keyword_location_counts.csv
-keyword_location_counts_file_name="${input_file_name/.csv/_keyword_location_counts.csv}"
-# Combine the output folder with the new file name
-keyword_location_counts_file_name_file_path="${output_file_folder}${keyword_location_counts_file_name}"
+# List to store files that need to be processed
+files_to_process_step1=()
+files_to_process_step2=()
 
-echo "Running count_keywords_per_article.py"
-python3 count_keywords_per_article.py --input_file_path $input_file_path --output_file_path $keyword_location_counts_file_name_file_path  --language $language
+# Get all file names in the input folder
+for file in "$input_folder"/*; do
+  # Extract the file name (without the folder path)
+  filename=$(basename "$file")
+
+  # Check if file already processed in step 1
+  filename_processed_step1="${filename/.csv/_counts_per_article.csv}"
+  # Check if the processed version exists in the processed folder
+  if [ ! -f "$output_processing_1_folder/$filename_processed_step1" ]; then
+   # If not, add the file to the list for processing
+    files_to_process_step1+=("$filename")
+  fi
+  
+
+  # Check if file already processed in step 2
+  filename_processed_step2="${filename/.csv/_counts_per_day.csv}"
+  # Check if the processed version exists in the processed folder
+  if [ ! -f "$output_processing_2_folder/$filename_processed_step2" ]; then
+   # If not, add the file to the list for processing
+    files_to_process_step2+=("$filename")
+  fi
+
+done
 
 
 #################################################
-# 2. daily_count_articles_mentioning_keywords.py
+# 2. Count keywords per article 
 #################################################
 
-# Replace .csv with _articles_mentioning_count.csv
-articles_mentioning_count_file_name="${input_file_name/.csv/_articles_mentioning_count.csv}"
-# Combine the output folder with the new file name
-articles_mentioning_count_file_path="${output_file_folder}${articles_mentioning_count_file_name}"
+# Processing step 1 each file in the list
+for filename in "${files_to_process_step1[@]}"; do
+  echo "Count keywords per article for $filename..."
 
-echo "Running daily_count_articles_mentioning_keywords.py"
-python3 daily_count_articles_mentioning_keywords.py --input_file_path $keyword_location_counts_file_name_file_path --output_file_path $articles_mentioning_count_file_path  --language $language
+  # Initialize the language variable
+  language=""
+
+  # Check if the file name contains "eng" or "ara"
+  if [[ "$filename" == *"eng"* ]]; then
+    language="English"
+  elif [[ "$filename" == *"ara"* ]]; then
+    language="Arabic"
+  else
+    echo "File '$filename' must contain either 'eng' or 'ara'. Skipping..."
+    continue  # Skip this file and move to the next
+  fi
+
+  # Output the determined language for the file
+  echo "File '$filename' is in language: $language"
+  
+  # Combine the file name with the folder path
+  input_file_path="$input_folder/$filename"
+
+  # Replace .csv with _counts_per_article.csv and create new file path
+  counts_per_article_file_name="${filename/.csv/_counts_per_article.csv}"
+  counts_per_article_file_name_file_path="${output_processing_1_folder}/${counts_per_article_file_name}"
+
+  python3 count_keywords_per_article.py --input_file_path $input_file_path --output_file_path $counts_per_article_file_name_file_path  --language $language
+
+done
 
 
-#################################################
-# 2. Manually concatenate the new daily count articles to the existing file
-#################################################
+#####################################################
+# 3. Accumulate keyword counts per day and location
+#####################################################
 
-# It is also important to concatenate the English and Arabic files together
+# Process each file in the list
+for filename in "${files_to_process_step2[@]}"; do
+   echo "Accumulate keyword counts per day and location for $filename..."
+
+  # Initialize the language variable
+  language=""
+
+  # Check if the file name contains "eng" or "ara"
+  if [[ "$filename" == *"eng"* ]]; then
+    language="English"
+  elif [[ "$filename" == *"ara"* ]]; then
+    language="Arabic"
+  else
+    echo "File '$filename' must contain either 'eng' or 'ara'. Skipping..."
+    continue  # Skip this file and move to the next
+  fi
+
+  # Output the determined language for the file
+  echo "File '$filename' is in language: $language"
+  
+  # Replace .csv with _counts_per_article.csv and create new file path
+  counts_per_article_file_name="${filename/.csv/_counts_per_article.csv}"
+  counts_per_article_file_name_file_path="${output_processing_1_folder}/${counts_per_article_file_name}"
+
+  # Replace .csv with _counts_per_article.csv and create new file path
+  counts_per_day_file_name="${filename/.csv/_counts_per_day.csv}"
+  counts_per_day_file_name_file_path="${output_processing_2_folder}/${counts_per_day_file_name}"
+
+  python3 count_keywords_per_day_location.py --input_file_path $counts_per_article_file_name_file_path --output_file_path $counts_per_day_file_name_file_path  --language $language
+
+done
+
+
+#####################################################
+# 3. Concatenate all processed files
+#####################################################
+
+# Check if the array files_to_process has any elements
+if [ ${#files_to_process_step2[@]} -gt 0 ]; then
+  echo "There were ${#files_to_process_step2[@]} files for which the daily counts were created in this update."
+
+  python3 concatenate_files.py --input_file_path $output_processing_2_folder --output_file_path $final_output_folder
+
+else
+  echo "No files to process."
+fi
+
+echo "Monthly update processing complete."
